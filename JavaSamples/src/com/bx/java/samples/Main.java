cc23ae9e3cafaff6205e67a18f93cc9763e84f04
Eduardo Medina <emedinaa@gmail.com>
2017-01-04 09:25:42 -0500

    add new java project

---
+++ JavaSamples/src/com/bx/java/samples/Main.java
@@ -0,0 +1,13 @@
+package com.bx.java.samples;
+
+/**
+ * Created by eduardomedina on 4/01/17.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello Java !");
+    }
+
+}
